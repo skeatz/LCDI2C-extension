3993baf6a002ee4eda3909910e661979df33f023
Max <sonthaya.nt@hotmail.com>
2021-01-18 21:29:07 +0700

    Add support IPST-WiFi

--- modules/LCDI2C.py
+++ modules/LCDI2C.py
@@ -1,6 +1,11 @@
 from machine import Pin, I2C
 
-i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+machine = os.uname().machine
+if "KidBright32" in machine:
+    i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+else:
+    i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
+
 
 LCDI2C_ADDR = 0x3F
 
